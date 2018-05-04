b148f77b5fce6ddc05a8a6763ba3a4456d6aace9
杨飞 <yangfei@hudongpai.com>
2018-05-04 16:53:01 +0800

    todo app

---
+++ src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <todo-list v-bind:todos="todos"/>
+    <create-todo v-on:create-todo="createTodo"/>
+  </div>
+</template>
+
+<script>
+import sweetalert from 'sweetalert';
+import TodoList from './components/TodoList';
+import CreateTodo from './components/CreateTodo';
+
+// import HelloWorld from './components/HelloWorld';
+
+
+export default {
+  name: 'App',
+  components: {
+    TodoList,
+    CreateTodo,
+  },
+  // data function avails data to the template
+  data() {
+    return {
+      todos: [{
+        title: 'Todo A',
+        project: 'Project A',
+        done: false,
+      }, {
+        title: 'Todo B',
+        project: 'Project B',
+        done: true,
+      }, {
+        title: 'Todo C',
+        project: 'Project C',
+        done: false,
+      }, {
+        title: 'Todo D',
+        project: 'Project D',
+        done: false,
+      }],
+    };
+  },
+
+  methods: {
+    createTodo(newTodo) {
+      this.todos.push(newTodo);
+      sweetalert('Success!', 'To-Do created!', 'success');
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
